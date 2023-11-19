f53a6094a544da0e709648469c108be36cbf6405
Pedro Vieira <pedro258vf@gmail.com>
2023-11-19 18:58:59 -0300

    arrumado o joi

--- src/utils/userJoiValidation.js
+++ src/utils/userJoiValidation.js
@@ -1,19 +1,19 @@
 const Joi = require('joi');
 
 const userSchema = Joi.object({
-  displayName: Joi.string().min(8).isRequired().message({
+  displayName: Joi.string().min(8).required().message({
     'string.min': '"displayName" length must be at least 8 characters long',
   }),
-  email: Joi.string().email().isRequired().message({
+  email: Joi.string().email().required().message({
     'string.email': '"email" must be a valid email',
   }),
-  password: Joi.string().min(6).isRequired.message({
+  password: Joi.string().min(6).required().message({
     'string.min': '"password" length must be at least 6 characters long',
   }),
   image: Joi.string(),
 });
 
-// validações ficam em utils?
+// isso ficaria em utils?
 // meus erros estão certos assim?
 
 module.exports = {
